385dd1ad878b61d56bd361e6d82615a8f465cf06
Tom Moran <bobtom8482gm@gmail.com>
2021-11-11 15:09:08 +0000

    new env vars

--- react-ui/src/pages/Login.js
+++ react-ui/src/pages/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+require('dotenv').config();
 
 const Login = () => {
-  const REDIRECT_URI = 'https://intense-eyrie-61400.herokuapp.com/main';
-  const LOCAL_REDIRECT_URI = 'http://localhost:3001/main';
-  const AUTH_URL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT}&scope=user-read-private%20user-read-email&redirect_uri=${LOCAL_REDIRECT_URI}&show_dialog=false`;
+  // const REDIRECT_URI = 'https://intense-eyrie-61400.herokuapp.com/main';
+  // const LOCAL_REDIRECT_URI = 'http://localhost:3001/main';
+  const AUTH_URL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=user-read-private%20user-read-email&redirect_uri=${process.env.REDIRECT_URI}&show_dialog=false`;
 
   return (
     <Container
